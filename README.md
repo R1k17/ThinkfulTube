38cd72be6d0be03063c8d9bc7e4abaa1b2bfb924
Tarik Mey <tarik-mey@gmx.de>
2018-03-13 21:28:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+===============================
+======== Documentation ========
+===============================
+
+==== Endpoint ====
+==================
+
+https://www.googleapis.com/youtube/v3/search
+
+==== Using the API key ====
+===========================
+
+AIzaSyBac9cFNnhK6shArenZzYFA742-SBGSVM0
+
+
+==== Desgin ====
+================
+
+App will have a centered searchbar located in the top area of the screen.
+Next to the searchbar the user will find a search button to submit the search keywords
+On the top left corner is the Thinkful Tube logo located.
+
+I want to render 10 results, 5 per row right under the search bar. For reference see 
+youtube.
+
+
+==== Solution example ====
+==========================
+
+https://repl.it/@bcrews/Thinkful-Tube
+
+
+
+
+// worked with the following links! USE the first one to continue with your current work!!!!
+
+https://developers.google.com/youtube/v3/docs/search/list
+
+https://developers.google.com/youtube/v3/docs/search
+
+https://www.youtube.com/watch?v=-vH2eZAM30s
+
+
+
+"https://www.googleapis.com/youtube/v3/search"
+
+
+
+Questions
+
+What means in:name ? >> jQuery AJAX example
+
+When to use ${} and $() ? 
---
+++ index.js
@@ -0,0 +1,47 @@
+// Step 1 - endpoint url
+const searchUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+// Step 2 - get data from API
+function getApiData(searchTerm, callback){
+    const query = {
+      part: 'snippet',
+      q: searchTerm,     
+      type: 'video',
+      key: 'AIzaSyBac9cFNnhK6shArenZzYFA742-SBGSVM0'
+    }
+    $.getJSON(searchUrl, query, callback);
+  }
+  
+// Step 3 - define the html structure for the data
+const resultsTemplate = (
+  '<div class="cssResults">'+'<p class="resultTitle"></p>'+'<a class="js_image_link" href=""><img class="js_image" src="" alt=""></a>'+'</div>'
+);
+
+// Step 4 - Render the data in the template
+function renderResults(result){
+  let template = $(resultsTemplate);
+  template.find('.js_image_link').attr('href', `https://www.youtube.com/watch?v=${result.id.videoId}`);
+  template.find('.js_image').attr('src', result.snippet.thumbnails.medium.url);
+  template.find('.resultTitle').text(result.snippet.title);
+  return template;
+}
+
+// Step 4 - displaying the data
+function displayData(data){
+  let results = data.items.map(function(item){ 
+      return renderResults(item);
+  });
+  $('#searchContainer').html(results);
+};
+
+// Step 5 - watch for submitting
+function searchSubmit(){
+$('.searchForm').submit(function(event){
+  event.preventDefault();
+  let queryTarget = $(event.currentTarget).find('#searchTerm');
+  let query = queryTarget.val();
+  getApiData(query, displayData);
+})
+};
+
+$(searchSubmit);
